b5709f3bafd0cbe4a9c4f2db7620f3ed6db5d4e8
Avneesh Mishra <114160711+miavneesh@users.noreply.github.com>
2024-03-15 14:21:51 +0000

    while and do while loop

---
+++ 02_chapter/03_iterations.js
@@ -0,0 +1,32 @@
+/// While and Do-WHile loop ///
+
+//while loop
+// let index = 0
+// while (index<=10) {
+   
+//     console.log(index);
+//     index++
+    
+// }
+
+//with array =>
+
+// let myState=["MP","UP","Punjab","Delhi","AP"]
+// let arr = 0
+// while (arr<myState.length) {
+//     console.log(`Given States Are -> ${myState[arr]}`);
+//     arr++
+// }
+
+
+//*****  Do while loop =>
+
+// let temp = 24
+
+// do {
+//     console.log(`temp is ${temp}`);
+//     temp = temp + 1;
+// } while (temp<=29);
+// in do while, the loop is executed at least one time in case when it does not matches with the condition
+
+
